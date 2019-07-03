b95f8abefaaecf9f61283526de0ac7e068414392
Ishu Jain <ishu.jain@BlueJeansNetwork.onmicrosoft.com>
2019-07-03 15:40:48 +0530

    changes for binary and unary operator overloading

---
+++ OperatorOverloading/BinaryOperatorOverloading.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperator
+{
+    //SYNTAX
+    //access specifier  className operator Operator_symbol (parameters)
+    //{
+    //    // Code
+    //}
+    class MyMathClassBinary
+    {
+        public int number1 = 0;
+        public MyMathClassBinary(int num1)
+        {
+            number1 = num1;
+        }
+        public static MyMathClassBinary operator +(MyMathClassBinary mathClass1, MyMathClassBinary mathClass2)
+        {
+            var mathClass3 = new MyMathClassBinary(0);
+            mathClass3.number1 = mathClass1.number1 + mathClass2.number1;
+            return mathClass3;
+
+        }
+        public void Display()
+        {
+            Console.WriteLine("number1 : " + number1);
+        }
+    }
+    class BinaryOperatorOverloading
+    {
+        static void Main(string[] args)
+        {
+            var math1 = new MyMathClassBinary(100);
+            var math2 = new MyMathClassBinary(400);
+            var math3 = new MyMathClassBinary(0);
+            math1.Display();
+            math2.Display();
+            math3.Display();
+            math3 = math1 + math2;
+            math1.Display();
+            math2.Display();
+            math3.Display();
+        }
+    }
+}
---
+++ OperatorOverloading/UnraryOperatorOverloading.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperator
+{
+    //SYNTAX
+    //access specifier  className operator Operator_symbol (parameters)
+    //{
+    //    // Code
+    //}
+
+        //UNARY OPERATOR
+    class MyMathClass
+    {
+        public int number1 = 0;
+        public int number2 = 0;
+        public MyMathClass(int num1, int num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+        public static MyMathClass operator -(MyMathClass myMathClass)
+        {
+            myMathClass.number1 = -myMathClass.number1;
+            myMathClass.number2 = -myMathClass.number2;
+            return myMathClass;
+        }
+        public void Display()
+        {
+            Console.WriteLine("number1 : " + number1);
+            Console.WriteLine("number2 : " + number2);
+        }
+    }
+    class UnraryOperatorOverloading
+    {
+        static void RemoveMe_Main(string[] args)
+        {
+            var math = new MyMathClass(123, -989);
+            math.Display();
+            math = -math;
+            math.Display();
+        }
+    }
+}
